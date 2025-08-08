7472e96ca1f3e6c35d39d1b50623f2cdaf9ed4de
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 21:52:42 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretWordGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string secretWord = "open";
+            string userGuess;
+            bool isCorrect = false;
+
+            do
+            {
+                Console.Write("Enter your guess: ");
+                userGuess = Console.ReadLine();
+
+                if (string.Equals(userGuess, secretWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                }
+            } while (!isCorrect);
+            Console.WriteLine("Correct!");
+        }
+    }
+}
